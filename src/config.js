19fd69c95f086f267114c799b02ca8796cbd7864
Juan Baltazar <juan.baltazar1@gmail.com>
2020-04-10 17:42:30 -0700

    changed db variable name

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL || 'postgresql://tubidaor@localhost/chatter',
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://tubidaor@localhost/chatter',
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '15m'
 }
