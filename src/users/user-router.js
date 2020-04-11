ade1a045868f6a839ca1160c079de723bc6b6fa7
Juan Baltazar <juan.baltazar1@gmail.com>
2020-04-10 22:34:15 -0700

    cleaned up code on user router

--- src/users/user-router.js
+++ src/users/user-router.js
@@ -2,7 +2,6 @@ const express = require('express');
 const UsersService = require('./user-service');
 const jsonBodyParser = express.json();
 const path = require('path');
-const requireAuth = require('../middleware/jwt-auth');
 
 
 const usersRouter = express.Router();
@@ -56,26 +55,4 @@ usersRouter
       .catch(next)
   })
 
-  // usersRouter
-  //   .route('/:user_id')
-  //   .all(requireAuth)
-  //   .get(getUserProfile)
-
-  //   async function getUserProfile(req, res, next) {
-  //     try {
-  //       const user = await UsersService.userNameExists(
-  //         req.app.get('db'),
-  //         res.params.user_id
-  //       )
-  //     if(!user)
-  //       return res.status(404).json({
-  //         error: `Thing doesn't exist`
-  //       })
-
-  //       res.user = user
-  //       next()
-  //   } catch(error) {
-  //     next(error)
-  //   }
-  // }
   module.exports = usersRouter
