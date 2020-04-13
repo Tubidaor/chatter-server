7e1d2756d873457b943a63f7db1aa4fddc76e819
Juan Baltazar <juan.baltazar1@gmail.com>
2020-04-13 16:45:01 -0700

    finished validating for children endpoint

--- test/children.spec.js
+++ test/children.spec.js
@@ -4,39 +4,47 @@ const helpers = require('./test-helpers');
 
 
 describe('get request for children data', () => {
-  let dataBase = knex({
+  let database = knex({
     client: 'pg',
     connection: process.env.TEST_DATABASE_URL,
   })
 
-  app.set('db', dataBase)
+  app.set('db', database)
 
   const testChildren = helpers.makeChildrenArray()
   const testUsers = helpers.makeUsersArray()
 
   before('make knex instance', () => {
-    dataBase
+    database
   })
-
-  after('disconnect database', () => dataBase.destroy())
+  before('clean up tables', () => helpers.cleanTables(database))
+  after('disconnect database', () => database.destroy())
+  afterEach('cleanup', () => helpers.cleanTables(database))
 
   context('requests children data', () => {
-    before('load children', () => {
-      helpers.seedUsers(dataBase, testUsers)
-      helpers.seedChildren(dataBase, testChildren)
+    beforeEach('load children', () => {
+      helpers.seedUsers(database, testUsers)
+      helpers.seedChildren(database, testChildren)
       
     })
-    // beforeEach('cleanup', () => helpers.cleanTables(dataBase))
-    afterEach('cleanup', () => helpers.cleanTables(dataBase))
 
     it('validate request for children data', () => {
       
       const user_name = testUsers[0].user_name
-
+      const childName = testChildren[0].name_
+      const childId = 1
+    
       return supertest(app)
         .get(`/api/children/${user_name}`)
         .set('Authorization', helpers.makeAuthHeader(testUsers[0]))
         .expect(200)
+        .expect(res => {
+          expect(res.body[0].id).to.eql(childId)
+          expect(res.body[0].name_).to.eql(childName)
+          expect(res.body).to.have.nested.property('[0].id')
+          expect(res.body).to.have.nested.property('[0].name_')
+
       })
+    })
   })
 })
